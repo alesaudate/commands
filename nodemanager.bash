86a297644079f06463089a46d696b20626e4c2e5
Alexandre Saudate <alesaudate@gmail.com>
2015-11-18 21:35:06 -0200

    Update nodemanager.bash

--- nodemanager.bash
+++ nodemanager.bash
@@ -1,1 +1,1 @@
-docker start nodemanager > /dev/null 2>&1 || docker run -i -v ~/weblogic12c:/opt/oracle/wlsdomains -p 5556:5556 -e TZ=+03:00 --link db --name=nodemanager sciensa2/soasuite12c-nodemanager
+docker start nodemanager > /dev/null 2>&1 || docker run -i -v ~/weblogic12c:/opt/oracle/wlsdomains -p 5556:5556 -e TZ=+03:00 --link db sciensa2/soasuite12c-nodemanager
