9499e1910fd076525e10621ba2ab83235442023e
Alexandre Saudate <alesaudate@gmail.com>
2015-11-19 19:22:13 -0200

    Update jdeveloper12c.bash

--- jdeveloper12c.bash
+++ jdeveloper12c.bash
@@ -1,2 +1,2 @@
 xhost + 
-docker run -d -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --link soasuite12c sciensa2/soasuite12c /opt/oracle/middleware12c/jdeveloper/jdev/bin/jdev
+docker run -i -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --link soasuite12c sciensa2/soasuite12c /opt/oracle/middleware12c/jdeveloper/jdev/bin/jdev
