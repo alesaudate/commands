a64ff35217164ce93dd6752bbb5d985e6b154fe0
Alexandre Saudate <alesaudate@gmail.com>
2015-11-19 14:14:38 -0200

    Update soaserver.bash

--- soaserver.bash
+++ soaserver.bash
@@ -1,1 +1,1 @@
-docker exec -d soasuite12c /opt/oracle/wlsdomains/domains/soa_domain/startManagedWebLogic.sh soa_server1
+docker exec -d soasuite12c /opt/oracle/wlsdomains/domains/soa_domain/bin/startManagedWebLogic.sh soa_server1
