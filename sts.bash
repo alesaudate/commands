bd425f9590d6cf21bbf22fe04930837a4614e9b1
Alexandre Saudate <alesaudate@gmail.com>
2015-11-23 11:41:09 -0200

    Update sts.bash

--- sts.bash
+++ sts.bash
@@ -2,4 +2,5 @@ xhost +
 mkdir -p ~/workspaceSTS
 USER=$USER
 sudo chown -R $USER:$USER ~/workspaceSTS
+sudo chmod 777 ~/workspaceSTS
 docker start STS > /dev/null 2>&1  || docker run --name "STS" -it -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/workspaceSTS:/home/developer/workspaceSTS funthomas424242/spring-sts-ide.docker
