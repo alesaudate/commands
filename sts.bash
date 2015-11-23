2b91dcf18c21c71c1b034821fbd9a94a6be98af1
Alexandre Saudate <alesaudate@gmail.com>
2015-11-22 22:17:26 -0200

    Update sts.bash

--- sts.bash
+++ sts.bash
@@ -1,2 +1,2 @@
 xhost +
-docker run --name "STS" -it -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix funthomas424242/spring-sts-ide.docker
+docker start STS > /dev/null 2>&1  || docker run --name "STS" -it -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix funthomas424242/spring-sts-ide.docker
