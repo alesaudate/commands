a62e79fded57885a8589f3cf2ad1ce467309a5ff
Alexandre Saudate <alesaudate@gmail.com>
2015-11-24 14:59:16 -0200

    Update jenkins.bash

--- jenkins.bash
+++ jenkins.bash
@@ -1,3 +1,5 @@
 mkdir -p $HOME/jenkins
 chmod 777 $HOME/jenkins
+useradd jenkins
+sudo chown -R jenkins:jenkins $HOME/jenkins
 docker run -p 8080:8180 -p 50000:50000 -v $HOME/jenkins:/var/jenkins_home jenkins
