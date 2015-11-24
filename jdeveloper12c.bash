c461adca4198dfc8a33da3b247916ca79c3b16f6
Alexandre Saudate <alesaudate@gmail.com>
2015-11-24 16:58:46 -0200

    Update jdeveloper12c.bash

--- jdeveloper12c.bash
+++ jdeveloper12c.bash
@@ -1,3 +1,3 @@
 xhost + 
 mkdir ~/jdeveloper
-docker start jdeveloper12c > /dev/null 2>&1  || docker run -i -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/jdeveloper:/home/developer/jdeveloper -t --link soasuite12c --name=jdeveloper12c sciensa2/jdeveloper12c
+docker start jdeveloper12c > /dev/null 2>&1  || docker run -d -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/jdeveloper:/home/developer/jdeveloper -t --link soasuite12c --name=jdeveloper12c sciensa2/jdeveloper12c
