6fa3001476af8eb15023f8a3ae9d957a2336b13b
Alexandre Saudate <alesaudate@gmail.com>
2015-11-24 17:29:04 -0200

    Update soasuite12c.bash

--- soasuite12c.bash
+++ soasuite12c.bash
@@ -1,7 +1,1 @@
-sudo useradd -u 1000 dockeruser
-mkdir -p ~/weblogic12c/logs 
-sudo chown -R dockeruser:dockeruser ~/weblogic12c/logs
-mkdir -p ~/weblogic12c/common
-sudo chown -R dockeruser:dockeruser ~/weblogic12c/common
-
 docker start soasuite12c > /dev/null 2>&1 || docker run -d -v ~/weblogic12c/common:/opt/oracle -v ~/weblogic12c:/opt/oracle/wlsdomains -v ~/weblogic12c/logs:/var/log/weblogic -e USER=$USER -e TZ=+03:00 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --link db -p 7001:7001 -p 8001:8001 -p 8011:8011 --name=soasuite12c sciensa2/soasuite12c  nohup /opt/oracle/wlsdomains/domains/soa_domain/startWebLogic.sh > /dev/null 2>&1 &
