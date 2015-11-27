2550b06409e7bff222ce6f80a90586d6661e3265
Alexandre Saudate <alesaudate@gmail.com>
2015-11-27 10:23:26 -0200

    Update dominio12c.bash

--- dominio12c.bash
+++ dominio12c.bash
@@ -1,5 +1,3 @@
-sudo useradd -u 1000 dockeruser
 mkdir -p ~/weblogic12c/domains
-sudo chown -R dockeruser:dockeruser ~/weblogic12c
 sudo chmod -R 777 ~/weblogic12c
 docker run -i -v ~/weblogic12c/domains:/opt/oracle/wlsdomains/domains -e USER=$USER --link db sciensa2/soasuite12c-domain
