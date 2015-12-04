ef1df05affbdabdc1b5f7afc65cb298974dc9b58
Alexandre Saudate <alesaudate@gmail.com>
2015-12-04 14:13:14 -0200

    Update jenkins.bash

--- jenkins.bash
+++ jenkins.bash
@@ -2,3 +2,4 @@ mkdir -p $HOME/jenkins
 sudo useradd -u 1000 jenkins
 sudo chown -R jenkins:jenkins $HOME/jenkins
 docker start jenkins > /dev/null 2>&1 || docker run --name jenkins -p 8180:8080 -p 50000:50000 -v $HOME/jenkins:/var/jenkins_home jenkins
+xdg-open http://localhost:8180/
